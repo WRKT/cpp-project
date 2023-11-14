60a240d470f9fcc8842190c347c583c0893ac0ed
Dinholu <alizeehett@hotmail.fr>
2023-11-14 21:12:05 +0100

    changement de la logique du jeu pour vérifier que tout fonctionne

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-#include "match.h"
+//#include "match.h"
+#include "grille.h"
+#include "joueurhumain.h"
+#include "joueurordinateur.h"
 
-int main()
-{
-    Match JeuV1;
-    JeuV1.StartJeu();
+//int main()
+//{
+//    Match JeuV1;
+//    JeuV1.StartJeu();
+//}
+
+
+int main() {
+    std::cout << std::endl;
+    Grille grille(3, 3);
+    grille.InitialiserGrille();
+
+    JoueurHumain joueur("Alice", Jeton::X);
+    JoueurOrdinateur joueur2(Jeton::O);
+
+    grille.AfficherGrille();
+    std::cout << "Bienvenue " + joueur.ObtenirNom() << std::endl;
+
+
+    return 0;
 }
-
