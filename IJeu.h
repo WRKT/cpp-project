09380e04b6c2884ba7a1c2c082c2fe6377018381
Dinholu <alizeehett@hotmail.fr>
2023-11-19 14:59:02 +0100

    CHG - PlacerJeton n'est plus propre a tous

--- IJeu.h
+++ IJeu.h
@@ -1,15 +1,12 @@
 #ifndef IJEU_H
 #define IJEU_H
 
-#include "jeton.h"
-
 class IJeu {
 public:
     virtual void Jouer() = 0;
     virtual void TourHumain() = 0;
     virtual void TourOrdi() = 0;
     virtual bool AGagne() const = 0;
-    virtual void PlacerJeton(int ligne, int colonne, Jeton jeton) = 0;
     virtual bool PartieFinie() const = 0;
     virtual ~IJeu() = default;
 };
