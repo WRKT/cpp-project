129e3751f083f7d2267d7977a7f575f825c6e3c3
Dinholu <alizeehett@hotmail.fr>
2023-11-21 20:17:39 +0100

    CHG de la factory jeu

--- jeufactory.cpp
+++ jeufactory.cpp
@@ -1,10 +1,13 @@
 #include "jeufactory.h"
 #include "jeumorpion.h"
 #include "jeupuissance4.h"
+#include "grillefactory.h"
+
 #include <memory>
 // Inclure les autres fichiers de jeux si nécessaire
 
-std::unique_ptr<IJeu> JeuFactory::CreerJeu(TypesJeu typeDeJeu, IGrille& grille, std::shared_ptr<IJoueur> joueur1, std::shared_ptr<IJoueur> joueur2) {
+std::unique_ptr<IJeu> JeuFactory::CreerJeu(TypesJeu typeDeJeu, std::shared_ptr<IJoueur> joueur1, std::shared_ptr<IJoueur> joueur2) {
+    std::shared_ptr<IGrille> grille = GrilleFactory::CreerGrille(typeDeJeu);
     switch (typeDeJeu) {
     case TypesJeu::Morpion:
         return std::make_unique<JeuMorpion>(grille, joueur1, joueur2);
