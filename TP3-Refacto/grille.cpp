eab24d6072552629a0a769a8c4861469cc77f7b3
Winness Rakotozafy <winness.rakotozafy@outlook.fr>
2023-11-23 21:59:16 +0100

    fix: apply const-correctness sur les paramètres de grille

--- TP3-Refacto/grille.cpp
+++ TP3-Refacto/grille.cpp
@@ -11,7 +11,7 @@ int Grille::getNbColonne() const {
     return nbColonnes;
 }
 
-Jeton Grille::GetCellule(int x, int y) const
+Jeton Grille::GetCellule(const int x, const int y) const
 {
     return table[x][y];
 }
@@ -41,11 +41,11 @@ void Grille::AfficherGrille() const {
 };
 
 
-bool Grille::ACaseVide(int x, int y) const {
+bool Grille::ACaseVide(const int x, const int y) const {
     return table[x][y] == Jeton::Vide;
 }
 
-void Grille::ChangeCellule(int x, int y, Jeton jeton) {
+void Grille::ChangeCellule(const int x, const int y, Jeton jeton) {
     table[x][y] = jeton;
 }
 
--- TP3-Refacto/grille.h
+++ TP3-Refacto/grille.h
@@ -14,11 +14,11 @@ public:
     void InitialiserGrille() override;
     void AfficherGrille() const override;
     bool EstRemplie() const override;
-    bool ACaseVide(int x, int y) const override;
+    bool ACaseVide(const int x, const int y) const override;
     int getNbLigne() const override;
     int getNbColonne() const override;
-    Jeton GetCellule(int x, int y) const override;
-    void ChangeCellule(int x, int y, Jeton jeton) override;
+    Jeton GetCellule(const int x, const int y) const override;
+    void ChangeCellule(const int x, const int y, Jeton jeton) override;
 
 };
 
