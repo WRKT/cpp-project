635a0d5b1c9fa8488edf4f1b819f426e0e4f5dfc
Winness Rakotozafy <winness.rakotozafy@outlook.fr>
2023-11-24 18:44:03 +0100

    CHORE - Suppression fichiers inutiles

--- TP3-Refacto/affichageconsole.cpp
+++
@@ -1,6 +0,0 @@
-#include "affichageconsole.h"
-
-AffichageConsole::AffichageConsole()
-{
-
-}
--- TP3-Refacto/joueurordinateur.cpp
+++
@@ -1,9 +0,0 @@
-#include "joueurordinateur.h"
-#include <chrono>
-
-JoueurOrdinateur::JoueurOrdinateur(Jeton jeton) : jeton(jeton), generateur(std::chrono::system_clock::now().time_since_epoch().count()) {}
-
-
-Jeton JoueurOrdinateur::getJeton() const {
-    return jeton;
-}
