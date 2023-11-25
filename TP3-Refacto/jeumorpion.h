151adea2a7f5d423654a696ab496364164b62698
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-25 19:29:15 +0100

    FIX - suppression de virtual dans classe enfant

--- TP3-Refacto/jeumorpion.h
+++ TP3-Refacto/jeumorpion.h
@@ -7,23 +7,23 @@
 #include <memory>
 
 class JeuMorpion : public IJeu {
+public:
+    JeuMorpion(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2);
+    void Jouer() override;
+    void TourHumain() override;
+    void TourOrdi() override;
+    bool AGagne() const override;
+    bool PartieFinie() const override;
+    void PlacerJeton(int ligne, int colonne, Jeton jeton);
+    bool VerifieLignes() const;
+    bool VerifieColonnes() const;
+    bool VerifieDiagonales() const;
+
 private:
     std::shared_ptr<IGrille> grille;
     std::shared_ptr<IJoueur> joueur1;
     std::shared_ptr<IJoueur> joueur2;
     std::shared_ptr<IJoueur> joueurCourant;
-
-public:
-    JeuMorpion(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2);
-    virtual void TourHumain() override;
-    virtual void TourOrdi() override;
-    virtual bool AGagne() const override;
-    virtual bool PartieFinie() const override;
-    void PlacerJeton(int ligne, int colonne, Jeton jeton);
-    bool VerifieLignes() const;
-    bool VerifieColonnes() const;
-    bool VerifieDiagonales() const;
-    void Jouer() override;
 };
 
 #endif // JEUMORPION_H
