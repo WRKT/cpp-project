ce76127654223f572854c7aad7ac050f7c059423
Winness R <winness.rakotozafy@outlook.fr>
2023-11-26 00:02:11 +0100

    FEAT - Include IAffichage pour la gestion des outputs

--- TP3-Refacto/jeupuissance4.h
+++ TP3-Refacto/jeupuissance4.h
@@ -1,20 +1,16 @@
 #ifndef JEUPUISSANCE4_H
 #define JEUPUISSANCE4_H
 
+#include "IAffichage.h"
 #include "IJeu.h"
 #include "IGrille.h"
 #include "IJoueur.h"
 #include <memory>
 
 class JeuPuissance4 : public IJeu {
-private:
-    std::shared_ptr<IGrille> grille;
-    std::shared_ptr<IJoueur> joueur1;
-    std::shared_ptr<IJoueur> joueur2;
-    std::shared_ptr<IJoueur> joueurCourant;
 
 public:
-    JeuPuissance4(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2);
+    JeuPuissance4(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2, std::shared_ptr<IAffichage> modeAffichage);
     void Jouer() override;
     void TourHumain() override;
     void TourOrdi() override;
@@ -24,6 +20,13 @@ public:
     bool VerifieLignes() const;
     bool VerifieDiagonales() const;
     bool VerifieColonnes() const;
+
+private:
+    std::shared_ptr<IGrille> grille;
+    std::shared_ptr<IJoueur> joueur1;
+    std::shared_ptr<IJoueur> joueur2;
+    std::shared_ptr<IJoueur> joueurCourant;
+    std::shared_ptr<IAffichage> modeAffichage;
 };
 
 #endif // JEUPUISSANCE4_H
