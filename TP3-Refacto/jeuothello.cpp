b83efd6951bd1b6da8e98145f1f93f9468b8b41f
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-25 20:06:36 +0100

    FEAT - ajout des méthodes

--- TP3-Refacto/jeuothello.cpp
+++ TP3-Refacto/jeuothello.cpp
@@ -6,3 +6,38 @@
 
 JeuOthello::JeuOthello(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2)
     : grille(grille), joueur1(j1), joueur2(j2), joueurCourant(j1) {}
+
+void JeuOthello::Jouer()
+{
+
+}
+
+void JeuOthello::TourHumain()
+{
+
+}
+
+void JeuOthello::TourOrdi()
+{
+
+}
+
+bool JeuOthello::AGagne() const
+{
+
+}
+
+bool JeuOthello::PartieFinie() const
+{
+
+}
+
+void JeuOthello::PlacerJeton(int colonne, Jeton jeton)
+{
+
+}
+
+void JeuOthello::voirCoupPossibles()
+{
+
+}
