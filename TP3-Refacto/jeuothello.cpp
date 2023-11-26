c02ef4dde8ca3d19a3e10c0b08ca0b873009800e
Vladimir Sacchetto <91117420+Vladimir9595@users.noreply.github.com>
2023-11-26 15:09:19 +0100

    FIX - ajout des limites

--- TP3-Refacto/jeuothello.cpp
+++ TP3-Refacto/jeuothello.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <random>
 #include <ctime>
+#include <limits>
 
 JeuOthello::JeuOthello(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2)
     : grille(grille), joueur1(j1), joueur2(j2), joueurCourant(j1) {}
