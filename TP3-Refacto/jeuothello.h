0412c36c7b9eb86db13c55459a5e9d75aac219a5
Dinholu <alizeehett@hotmail.fr>
2023-11-27 22:15:06 +0100

    CHG - ajout d'otello avec la version optimisé

--- TP3-Refacto/jeuothello.h
+++ TP3-Refacto/jeuothello.h
@@ -4,15 +4,16 @@
 #include "IGrille.h"
 #include "IJoueur.h"
 #include <memory>
+#include "IAffichage.h"
 
 class JeuOthello : public IJeu {
 public:
-    JeuOthello(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2);
+    JeuOthello(std::shared_ptr<IGrille> grille, std::shared_ptr<IJoueur> j1, std::shared_ptr<IJoueur> j2, std::shared_ptr<IAffichage> modeAffichage);
     void Jouer() override;
-    void TourHumain() override;
-    void TourOrdi() override;
+    void Tour() override;
     bool AGagne() const override;
     bool PartieFinie() const override;
+    std::vector<std::pair<int, int>> CoupsPossibles() override;
     void PlacerJeton(int x, int y, Jeton jeton);
     void VoirCoupsPossibles();
     bool VerifiePions() const;
@@ -26,6 +27,8 @@ private:
     std::shared_ptr<IJoueur> joueur1;
     std::shared_ptr<IJoueur> joueur2;
     std::shared_ptr<IJoueur> joueurCourant;
+    std::shared_ptr<IAffichage> modeAffichage;
+
 };
 
 #endif // JEUOTHELLO_H
