d92d62979f20efc9462239d8e9b9daf349570687
Dinholu <alizeehett@hotmail.fr>
2023-11-28 21:50:00 +0100

    FEAT - ajout de retourner jeton

--- TP3-Refacto/jeuothello.cpp
+++ TP3-Refacto/jeuothello.cpp
@@ -121,6 +121,22 @@ bool JeuOthello::EstDirectionValide(int x, int y, int dx, int dy, Jeton jeton) c
 
     return false;
 }
+void JeuOthello::RetournerJetons(const int x, const int y, Jeton jeton) {
+    for (int dx = -1; dx <= 1; ++dx) {
+        for (int dy = -1; dy <= 1; ++dy) {
+            if (dx == 0 && dy == 0) continue;
+            if (PeutRetourner(x, y, dx, dy, jeton)) {
+                int l = x + dx;
+                int c = y + dy;
+                while (grille->GetCellule(l, c) != jeton) {
+                    grille->ChangeCellule(l, c, jeton);
+                    l += dx;
+                    c += dy;
+                }
+            }
+        }
+    }
+}
 
 
 
