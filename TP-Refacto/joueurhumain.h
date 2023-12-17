55893ab9fbdc52e8acfa68b738938af06c210a78
Winness R <winness.rakotozafy@outlook.fr>
2023-12-17 21:07:25 +0100

    CHG - Suppression de l'implémentation de estHumain sur Joueur

--- TP-Refacto/joueurhumain.h
+++ TP-Refacto/joueurhumain.h
@@ -10,7 +10,6 @@ class JoueurHumain : public AJoueur
 
 public:
     JoueurHumain(const std::string &nom, Jeton jeton) : AJoueur(nom, jeton){};
-    bool estHumain() const override { return true; }
     std::pair<int, int> ChoisirCoupMorpion(std::vector<std::pair<int, int>> CoupsPossibles) override;
     std::pair<int, int> ChoisirCoupPuissance4(std::vector<std::pair<int, int>> CoupsPossibles) override;
     std::pair<int, int> ChoisirCoupOthello(std::vector<std::pair<int, int>> CoupsPossibles) override;
--- TP-Refacto/joueurordinateur.h
+++ TP-Refacto/joueurordinateur.h
@@ -2,7 +2,6 @@
 #define JOUEUR_ORDINATEUR_H
 
 #include "AJoueur.h"
-#include "jeton.h"
 #include <string>
 #include <random>
 #include <chrono>
@@ -12,7 +11,6 @@ class JoueurOrdinateur : public AJoueur
 
 public:
     JoueurOrdinateur(Jeton jeton) : AJoueur("Ordinateur", jeton), generateur(std::chrono::system_clock::now().time_since_epoch().count()){};
-    bool estHumain() const override { return false; }
     std::pair<int, int> ChoisirCoupMorpion(std::vector<std::pair<int, int>> CoupsPossibles) override;
     std::pair<int, int> ChoisirCoupPuissance4(std::vector<std::pair<int, int>> CoupsPossibles) override;
     std::pair<int, int> ChoisirCoupOthello(std::vector<std::pair<int, int>> CoupsPossibles) override;
