57d15217bca802f613179d3201dd37280113731e
Vladimir9595 <vladisac@hotmail.it>
2024-01-07 20:46:58 +0100

    FEAT - ajout des methodes pour le jeu des dames

--- TP-Refacto/jeudames.cpp
+++ TP-Refacto/jeudames.cpp
@@ -43,18 +43,105 @@ bool JeuDames::PartieFinie() const {
 
 std::vector<std::pair<int, int>> JeuDames::CoupsPossibles() {
     std::vector<std::pair<int, int>> coupsPossibles;
-    for (int colonne = 0; colonne < grille->getNbColonnes(); ++colonne) {
-        for (int ligne = grille->getNbLignes() - 1; ligne >= 0; --ligne) {
-            if (grille->GetCellule(ligne, colonne) == Jeton::Vide) {
-                coupsPossibles.emplace_back(ligne, colonne);
-                break;
+
+    for (int ligne = 0; ligne < grille->getNbLignes(); ++ligne) {
+        for (int colonne = 0; colonne < grille->getNbColonnes(); ++colonne) {
+            if (grille->GetCellule(ligne, colonne) == joueurCourant->getJeton()) {
+
+                if (PeutDeplacerEnDiagonale(ligne, colonne, ligne - 1, colonne - 1)) {
+                    coupsPossibles.emplace_back(ligne - 1, colonne - 1);
+                }
+                if (PeutDeplacerEnDiagonale(ligne, colonne, ligne - 1, colonne + 1)) {
+                    coupsPossibles.emplace_back(ligne - 1, colonne + 1);
+                }
+                if (PeutDeplacerEnDiagonale(ligne, colonne, ligne + 1, colonne - 1)) {
+                    coupsPossibles.emplace_back(ligne + 1, colonne - 1);
+                }
+                if (PeutDeplacerEnDiagonale(ligne, colonne, ligne + 1, colonne + 1)) {
+                    coupsPossibles.emplace_back(ligne + 1, colonne + 1);
+                }
+                AjouterCapturesPossibles(ligne, colonne, coupsPossibles);
             }
         }
     }
+
     return coupsPossibles;
+}
+
+
+bool JeuDames::PeutDeplacerEnDiagonale(int xDepart, int yDepart, int xArrivee, int yArrivee) const {
+    return grille->ACaseVide(xArrivee, yArrivee) &&
+           grille->GetCellule(xArrivee, yArrivee) == Jeton::Vide &&
+           std::abs(xArrivee - xDepart) == 1 &&
+           std::abs(yArrivee - yDepart) == 1;
+}
+
+void JeuDames::AjouterCapturesPossibles(int x, int y, std::vector<std::pair<int, int>>& coupsPossibles) const {
+    const Jeton jetonCourant = joueurCourant->getJeton();
+
+    if (grille->ACaseVide(x - 1, y - 1) && grille->GetCellule(x - 1, y - 1) != jetonCourant &&
+        grille->ACaseVide(x - 2, y - 2) && grille->GetCellule(x - 2, y - 2) == Jeton::Vide) {
+        coupsPossibles.emplace_back(x - 2, y - 2);
+    }
+
+    if (grille->ACaseVide(x - 1, y + 1) && grille->GetCellule(x - 1, y + 1) != jetonCourant &&
+        grille->ACaseVide(x - 2, y + 2) && grille->GetCellule(x - 2, y + 2) == Jeton::Vide) {
+        coupsPossibles.emplace_back(x - 2, y + 2);
+    }
+
+    if (grille->ACaseVide(x + 1, y - 1) && grille->GetCellule(x + 1, y - 1) != jetonCourant &&
+        grille->ACaseVide(x + 2, y - 2) && grille->GetCellule(x + 2, y - 2) == Jeton::Vide) {
+        coupsPossibles.emplace_back(x + 2, y - 2);
+    }
+
+    if (grille->ACaseVide(x + 1, y + 1) && grille->GetCellule(x + 1, y + 1) != jetonCourant &&
+        grille->ACaseVide(x + 2, y + 2) && grille->GetCellule(x + 2, y + 2) == Jeton::Vide) {
+        coupsPossibles.emplace_back(x + 2, y + 2);
+    }
 }
 
 void JeuDames::AfficherResultat() const {
     modeAffichage->AfficherMessage("Match nul !");
+}
+
+void JeuDames::DeplacerPiece(int xDepart, int yDepart, int xArrivee, int yArrivee) {
+    grille->ChangeCellule(xArrivee, yArrivee, joueurCourant->getJeton());
+    grille->ChangeCellule(xDepart, yDepart, Jeton::Vide);
+}
+
+bool JeuDames::EstDeplacementValide(int xDepart, int yDepart, int xArrivee, int yArrivee) const {
+    if (grille->GetCellule(xArrivee, yArrivee) != Jeton::Vide) {
+        return false;
+    }
+
+    int deltaX = std::abs(xArrivee - xDepart);
+    int deltaY = std::abs(yArrivee - yDepart);
+    if (deltaX != 1 || deltaY != 1) {
+        return false;
+    }
+
+    return true;
+}
+
+void JeuDames::CapturerPiece(int xDepart, int yDepart, int xArrivee, int yArrivee)
+{
+    if (EstDeplacementValide(xDepart, yDepart, xArrivee, yArrivee)) {
+        grille->ChangeCellule(xArrivee, yArrivee, joueurCourant->getJeton());
+        int xCapture = (xDepart + xArrivee) / 2;
+        int yCapture = (yDepart + yArrivee) / 2;
+        grille->ChangeCellule(xCapture, yCapture, Jeton::Vide);
+        positionsJoueurs[joueurCourant] = {xArrivee, yArrivee};
+    }
+}
+
+bool JeuDames::PeutCapturer(int x, int y) const
+{
+    const Jeton jetonCourant = joueurCourant->getJeton();
+
+    return (grille->ACaseVide(x, y) &&
+            ((grille->ACaseVide(x - 2, y - 2) && grille->GetCellule(x - 1, y - 1) != jetonCourant && grille->GetCellule(x - 2, y - 2) == Jeton::Vide) ||
+             (grille->ACaseVide(x - 2, y + 2) && grille->GetCellule(x - 1, y + 1) != jetonCourant && grille->GetCellule(x - 2, y + 2) == Jeton::Vide) ||
+             (grille->ACaseVide(x + 2, y - 2) && grille->GetCellule(x + 1, y - 1) != jetonCourant && grille->GetCellule(x + 2, y - 2) == Jeton::Vide) ||
+             (grille->ACaseVide(x + 2, y + 2) && grille->GetCellule(x + 1, y + 1) != jetonCourant && grille->GetCellule(x + 2, y + 2) == Jeton::Vide)));
 }
 
--- TP-Refacto/jeudames.h
+++ TP-Refacto/jeudames.h
@@ -6,6 +6,7 @@
 #include "AJoueur.h"
 #include "IAffichage.h"
 #include <memory>
+#include <unordered_map>
 
 class JeuDames : public IJeu {
 
@@ -25,7 +26,13 @@ private:
     std::shared_ptr<AJoueur> joueur2;
     std::shared_ptr<AJoueur> joueurCourant;
     std::shared_ptr<IAffichage> modeAffichage;
-
+    std::unordered_map<std::shared_ptr<AJoueur>, std::pair<int, int>> positionsJoueurs;
+    bool PeutDeplacerEnDiagonale(int xDepart, int yDepart, int xArrivee, int yArrivee) const;
+    void AjouterCapturesPossibles(int x, int y, std::vector<std::pair<int, int>>& coupsPossibles) const;
+    void DeplacerPiece(int xDepart, int yDepart, int xArrivee, int yArrivee);
+    bool EstDeplacementValide(int xDepart, int yDepart, int xArrivee, int yArrivee) const;
+    void CapturerPiece(int xDepart, int yDepart, int xArrivee, int yArrivee);
+    bool PeutCapturer(int x, int y) const;
 };
 
 #endif // JEUDAMES_H
