7e3cee08f9fe3b91d0a4c007f9f26ce6afab1b4f
Winness R <winness.rakotozafy@outlook.fr>
2024-01-17 18:32:47 +0100

    FEAT - Ajout sleep 2 secondes pour les coups Ordinateurs

--- TP-Refacto/joueurordinateur.cpp
+++ TP-Refacto/joueurordinateur.cpp
@@ -1,10 +1,13 @@
 #include "joueurordinateur.h"
-#include <vector>
+#include <thread>
 
 Position JoueurOrdinateur::ChoisirCoupMorpion(const std::vector<Position> &CoupsPossible)
 {
     std::uniform_int_distribution<int> distribution(0, CoupsPossible.size() - 1);
     int random = distribution(generateur);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
     return CoupsPossible[random];
 }
 
@@ -12,6 +15,9 @@ Position JoueurOrdinateur::ChoisirCoupPuissance4(const std::vector<Position> &Co
 {
     std::uniform_int_distribution<int> distribution(0, CoupsPossible.size() - 1);
     int random = distribution(generateur);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
     return CoupsPossible[random];
 }
 
@@ -19,6 +25,9 @@ Position JoueurOrdinateur::ChoisirCoupOthello(const std::vector<Position> &Coups
 {
     std::uniform_int_distribution<int> distribution(0, CoupsPossible.size() - 1);
     int random = distribution(generateur);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
     return CoupsPossible[random];
 }
 
@@ -26,5 +35,8 @@ Position JoueurOrdinateur::ChoisirCoupDames(const std::vector<Position> &CoupsPo
 {
     std::uniform_int_distribution<int> distribution(0, CoupsPossible.size() - 1);
     int random = distribution(generateur);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
     return CoupsPossible[random];
 }
